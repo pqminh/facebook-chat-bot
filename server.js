c716bcb5089f19638d7eaa227b9a71f831529b6b
minhpq <pqminh31100@gmail.com>
2016-09-17 20:32:43 +0700

    change port

--- server.js
+++ server.js
@@ -22,13 +22,13 @@ app.get('/', (req, res) => {
 });
 
 app.get('/webhook', function(req, res) {
-  if (req.query['hub.verify_token'] === 'anh_hoang_dep_trai_vo_doi') {
+  if (req.query['hub.verify_token'] === 'EAAPBWmdLchUBAKkZCYZCTzmBJYi8fRC53aPIIn1tZBadBv6D8kPOoqcX0ggnvAGZA0yh8lbXowPkVWSvFsR9ZCaa5MX3gHNMq66A1Q4r1MHpvFjamVFjwaEeDG7zBI3G91qOiWoCK3D5FjNHU2Ub4HFndkBBOZCrh2iFcaw1BGSAZDZD') {
     res.send(req.query['hub.challenge']);
   }
   res.send('Error, wrong validation token');
 });
 
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3000);
 app.set('ip', process.env.OPENSHIFT_NODEJS_IP || process.env.IP || "127.0.0.1");
 
 server.listen(app.get('port'), app.get('ip'), function() {
